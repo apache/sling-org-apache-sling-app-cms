1a76f0c2f29de42d85491f6f4e3757d4f8de8bb7
Dan Klco <dklco@apache.org>
2018-05-26 14:40:55 -0400

    Fixing an issue where the search found find un-published pages

--- reference/src/main/java/org/apache/sling/cms/reference/models/Search.java
+++ reference/src/main/java/org/apache/sling/cms/reference/models/Search.java
@@ -102,7 +102,7 @@ public class Search {
 
 		String term = Text.escapeIllegalXpathSearchChars(request.getParameter(TERM_PARAMETER)).replaceAll("'", "''");
 
-		String query = "SELECT * FROM [sling:Page] AS p WHERE (p.[jcr:content/hideInSitemap] IS NULL OR p.[jcr:content/hideInSitemap] <> true) AND ISDESCENDANTNODE(p, '"
+		String query = "SELECT * FROM [sling:Page] AS p WHERE [jcr:content/published]=true AND (p.[jcr:content/hideInSitemap] IS NULL OR p.[jcr:content/hideInSitemap] <> true) AND ISDESCENDANTNODE(p, '"
 				+ basePath + "') AND CONTAINS(p.*, '" + term + "') ORDER BY [jcr:score]";
 		log.debug("Searching for pages with {} under {} with query: {}", term, basePath, query);
 		Iterator<Resource> res = request.getResourceResolver().findResources(query, Query.JCR_SQL2);
