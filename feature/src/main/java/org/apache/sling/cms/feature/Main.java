ea52a3e0228fd42b5d1e63fa352eb78470ec92f5
Dan Klco <daniel.klco@gmail.com>
2020-07-25 11:57:39 -0400

    Adding current state on creating a standalone jar

---
+++ feature/src/main/java/org/apache/sling/cms/feature/Main.java
@@ -0,0 +1,47 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.sling.cms.feature;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Properties;
+import java.util.jar.Attributes;
+import java.util.jar.Manifest;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        System.out.println("Bootstraping Sling CMS Feature Model");
+        URL propertiesUrl =  Main.class.getClassLoader().getResource("slingcms.properties");
+        Properties properties = new Properties();
+        properties.load(propertiesUrl.openStream());
+
+        String version = properties.getProperty("version");
+        System.out.println("Version "+version);
+
+        URL farUrl = Main.class.getClassLoader().getResource("lib/org.apache.sling.cms.fmconverter-"+version+"-slingcms_far.far");
+        List<String> arguments = new ArrayList();
+        arguments.addAll(Arrays.asList(args));
+        if(!arguments.contains("-f")){
+            arguments.add("-f");
+            arguments.add(farUrl.toString());
+        }
+        org.apache.sling.feature.launcher.impl.Main.main(arguments.toArray(new String[arguments.size()]));
+    }
+}
