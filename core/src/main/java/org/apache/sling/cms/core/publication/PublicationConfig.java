0e0a9e7f5fcd8400fdce4aabc3eec6b07b0b879b
Dan Klco <dklco@apache.org>
2020-09-29 08:53:51 -0400

    Adding config titles / descriptions and updating the pub config to use dropdowns

--- core/src/main/java/org/apache/sling/cms/core/publication/PublicationConfig.java
+++ core/src/main/java/org/apache/sling/cms/core/publication/PublicationConfig.java
@@ -20,6 +20,7 @@ import org.apache.sling.cms.publication.INSTANCE_TYPE;
 import org.apache.sling.cms.publication.PUBLICATION_MODE;
 import org.osgi.service.metatype.annotations.AttributeDefinition;
 import org.osgi.service.metatype.annotations.ObjectClassDefinition;
+import org.osgi.service.metatype.annotations.Option;
 
 @ObjectClassDefinition(name = "%publication.config.name", description = "%publication.config.description", localization = "OSGI-INF/l10n/bundle")
 public @interface PublicationConfig {
@@ -27,9 +28,13 @@ public @interface PublicationConfig {
     @AttributeDefinition(name = "%publication.param.agents.name", description = "%publication.param.agents.description")
     String[] agents() default {};
 
-    @AttributeDefinition(name = "%publication.param.type.name", description = "%publication.param.type.description")
+    @AttributeDefinition(name = "%publication.param.type.name", description = "%publication.param.type.description", options = {
+            @Option(label = "Author", value = "AUTHOR"), @Option(label = "Renderer", value = "RENDERER"),
+            @Option(label = "Standalone", value = "STANDALONE") })
     INSTANCE_TYPE instanceType() default INSTANCE_TYPE.STANDALONE;
 
-    @AttributeDefinition(name = "%publication.param.mode.name", description = "%publication.param.mode.description")
+    @AttributeDefinition(name = "%publication.param.mode.name", description = "%publication.param.mode.description", options = {
+            @Option(label = "Content Distribution", value = "CONTENT_DISTRIBUTION"),
+            @Option(label = "Standalone", value = "STANDALONE") })
     PUBLICATION_MODE publicationMode() default PUBLICATION_MODE.STANDALONE;
 }
