eaa3fddf5137be5c686ca287051dac6b95574637
Dan Klco <klco@adobe.com>
2021-09-06 18:02:50 -0400

    Updating i18n and moving unneeded import of node-fetch

--- i18n-helper/src/index.js
+++ i18n-helper/src/index.js
@@ -14,7 +14,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-const fetch = require("node-fetch");
 const fs = require("fs");
 
 const languages = require("./languages");
