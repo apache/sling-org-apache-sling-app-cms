0dceb9d4752475eb4219a205d919c4813f48e179
Dan Klco <klco@adobe.com>
2022-04-17 00:03:19 -0400

    Minor - removing unused import

--- core/src/main/java/org/apache/sling/cms/core/models/StartContent.java
+++ core/src/main/java/org/apache/sling/cms/core/models/StartContent.java
@@ -42,7 +42,6 @@ import javax.jcr.query.Row;
 import org.apache.sling.api.SlingHttpServletRequest;
 import org.apache.sling.api.resource.Resource;
 import org.apache.sling.api.resource.ResourceResolver;
-import org.apache.sling.cms.CMSConstants;
 import org.apache.sling.models.annotations.Model;
 import org.osgi.annotation.versioning.ProviderType;
 import org.slf4j.Logger;
