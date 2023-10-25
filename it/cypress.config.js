89d45ebb3beb42ffccea353e9eecf38b7f325900
Dan Klco <klco@adobe.com>
2023-10-25 18:34:20 -0400

    Increasing test timeout

--- it/cypress.config.js
+++ it/cypress.config.js
@@ -21,6 +21,7 @@ const { pa11y, prepareAudit } = require("@cypress-audit/pa11y");
 
 module.exports = defineConfig({
   e2e: {
+    defaultCommandTimeout: 10000,
     baseUrl: "http://localhost:8080",
     viewportWidth: 1000,
     viewportHeight: 660,
